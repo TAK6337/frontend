5bcfc227d2e1a4596576ea97e8925cbd1c532d69
TAK6337 <107523641+TAK6337@users.noreply.github.com>
2024-07-12 17:04:58 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
+import Signup from './components/Signup';
+import PrivacyAgreement from './components/PrivacyAgreement';
 
 function App() {
+    const [agreed, setAgreed] = useState(false);
   return (
       <Router>
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/Login" element={<Login />} />
+              <Route path="/signup" element={<Signup agreed={agreed} />} />
+              <Route path="/PrivacyAgreement" element={<PrivacyAgreement setAgreed={setAgreed} />} />
           </Routes>
       </Router>
   );
